33e52dd55e43ad7304484488a121a42ef5e1c899
NicolasPozzer <pozzernico@gmail.com>
2024-01-11 13:59:03 -0300

    challenge done!

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,19 +1,19 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Solution {
+
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner teclado =new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1 = teclado.next();
+            int x = teclado.nextInt();
+            System.out.printf("%-15s%03d\n", s1, x);
 
-        // Press F11 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+F5 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
         }
+        System.out.println("================================");
     }
 }
