771aedc77e7a6d80659e543cf3935a206bb8f855
NicolasPozzer <pozzernico@gmail.com>
2025-03-26 17:47:28 -0300

    Update: hackerRank

---
+++ py-resolves/if-else.py
@@ -0,0 +1,9 @@
+n1 = int( input("Ingrese el primer numero: "))
+n2  = int( input("Ingrese el segundo numero: "))
+
+suma = n1 + n2
+
+if(suma >= 10):
+    print("El numero es mayor a 10")
+else:
+    print("El numero es menor a 10")
